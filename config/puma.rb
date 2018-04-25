06491fe6caee36ab31134a7eca03724713b3e2d7
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 17:12:39 -0400

    20 threads

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 # pidfile "/var/run/puma/my_app.sock"
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 10)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 20)
 threads threads_count, threads_count
 
 preload_app!
