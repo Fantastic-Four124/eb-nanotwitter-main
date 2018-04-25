59f21c0a6fab9e23867fc0927860c50b2cba8e37
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 17:01:50 -0400

    sock comment

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
+# bind "unix:///var/run/puma/my_app.sock"
+# pidfile "/var/run/puma/my_app.sock"
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
