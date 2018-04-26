708989a2abaffdb964cf70c5423995b37c8d6aef
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 20:30:17 -0400

    5 threads

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@
 
 require 'connection_pool'
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 10)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
