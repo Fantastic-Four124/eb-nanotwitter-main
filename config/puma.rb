fad2a4957e42940f2e6640a60992cbd58d66a65d
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 20:35:42 -0400

    3 workers

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 # pidfile "/var/run/puma/my_app.sock"
 
 require 'connection_pool'
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
