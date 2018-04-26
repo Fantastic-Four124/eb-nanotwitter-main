af34de3005c8eca04cf2abbadb13bb7d5982aee2
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 22:27:32 -0400

    redis offline?

--- config/puma.rb
+++ config/puma.rb
@@ -15,9 +15,16 @@ environment ENV['RACK_ENV'] || 'development'
 tweet_uri_1 = URI.parse(ENV["TWEET_REDIS_URL"])
 tweet_uri_2 = URI.parse(ENV['TWEET_REDIS_SPARE_URL'])
 tweet_uri_3 = URI.parse(ENV['TWEET_REDIS_3'])
-$tweet_redis_1 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_1.host, :port => tweet_uri_1.port, :password => tweet_uri_1.password) }
-$tweet_redis_2 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_2.host, :port => tweet_uri_2.port, :password => tweet_uri_2.password) }
-$tweet_redis_3 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_3.host, :port => tweet_uri_3.port, :password => tweet_uri_3.password) }
+
+$tweet_redis_1 = Redis.new(:host => tweet_uri_1.host, :port => tweet_uri_1.port, :password => tweet_uri_1.password)
+
+$tweet_redis_2 = Redis.new(:host => tweet_uri_2.host, :port => tweet_uri_2.port, :password => tweet_uri_2.password)
+
+$tweet_redis_3 = Redis.new(:host => tweet_uri_3.host, :port => tweet_uri_3.port, :password => tweet_uri_3.password)
+
+#$tweet_redis_1 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_1.host, :port => tweet_uri_1.port, :password => tweet_uri_1.password) }
+#$tweet_redis_2 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_2.host, :port => tweet_uri_2.port, :password => tweet_uri_2.password) }
+#$tweet_redis_3 = ConnectionPool.new(size: 10) { Redis.new(:host => tweet_uri_3.host, :port => tweet_uri_3.port, :password => tweet_uri_3.password) }
 #$redis_num = 0
 
 on_worker_boot do
