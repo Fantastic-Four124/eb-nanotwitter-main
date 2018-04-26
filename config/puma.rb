96ccb8808a39fd32c8d489611be20d683f54b763
Tom Willkens <tom.willkens@gmail.com>
2018-04-25 22:31:45 -0400

    redis offline?

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 # bind "unix:///var/run/puma/my_app.sock"
 # pidfile "/var/run/puma/my_app.sock"
 
+require 'redis'
 require 'connection_pool'
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 10)
